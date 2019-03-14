19f62cafd2ef90de8b844620ac8b14ec7d422332
NeedPainkillers <apcxop@gmail.com>
2019-03-14 22:08:08 +0300

    up

---
+++ AvitoParcer/AvitoPost.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AvitoParcer
+{
+
+    static class By
+    {
+        //public static string itemProp = "itemprop\\s*=\\s*\"name\".*?>(.*?)<";
+        //public static string className = "class\s*=\s*\"(?:price|price+\s.*?)\".*?>(.*?)<";
+        //public static string parametr = "data-absolute-date\\s*=\\s*\"(.*?)\"";
+        //class\s*=\s*\"(?:item item_table|item item_table+\s.*?)\".*?>.*?(?:div class="js-item-date c-2|div class="js-item-date c-2+\s.*?).*?</div>
+
+        public static string ItemProp(string itemProp)
+        {
+            return "itemprop\\s*=\\s*\"(?:" + itemProp + "|" + itemProp + "+\\s.*?)\".*?>(.*?)<";
+        }
+        public static string ClassName(string className)
+        {
+            return "class\\s*=\\s*\"(?:" + className + "|" + className + "+\\s.*?)\".*?>(.*?)<";
+        }
+        public static string Parametr(string parametr)
+        {
+            return parametr + "\\s*=\\s*\"(.*?)\"";
+        }
+        public static string regexForPosts = "class\\s*=\\s*\"(?:item item_table|item item_table+\\s.*?)\".*?>.*?(?:div class=\"js-item-date c-2|div class=\"js-item-date c-2+\\s.*?).*?</div>";
+        public static string regexForImageUrls = "style=\"background-image: url\\(//(.*?)\\)\"" ;
+    }
+    static class Parce
+    {
+        //private static int FindStartInd(string data, string subStr)
+        //{
+        //    return (data.IndexOf(subStr) + subStr.Length);
+        //}
+        //private static int FindStartInd(string data, string subStr, int indexStart)
+        //{
+        //    return (data.IndexOf(subStr, indexStart) + subStr.Length);
+        //}
+        //private static int FindEndInd(string data, string subStr, int indexStart)
+        //{
+        //    return data.IndexOf(subStr, indexStart);
+        //}
+        //private static string FormSubString(string data, int indexStart, int indexEnd)
+        //{
+        //    return data.Substring(indexStart, indexEnd - indexStart);
+        //}
+        //public static string FindSubString(string data, string start, string end)
+        //{
+        //    int indexStart = FindStartInd(data, start);
+        //    return FormSubString(data, indexStart, FindEndInd(data, end, indexStart));
+        //}
+
+        //public static GroupCollection FindElements(string data, string regex)
+        //{
+        //    return Regex.Match(data, regex, RegexOptions.Singleline).Groups;
+        //}
+        public static GroupCollection FindElement(this String data, string regex)
+        {
+            return Regex.Matches(data, regex, RegexOptions.Singleline)[0].Groups;
+        }
+        public static MatchCollection FindElements(this String data, string regex)
+        {
+            return Regex.Matches(data, regex, RegexOptions.Singleline);
+        }
+    }
+
+
+
+    class AvitoPost
+    {
+        string name;
+        string price;
+        string additionalInfo;
+        string date;
+        string link;
+        List<string> imgLinks;
+        
+
+        public string Name { get => name; set => name = value; }
+        public string Price { get => price; set => price = value; }
+        public string AdditionalInfo { get => additionalInfo; set => additionalInfo = value; }
+        public string Date { get => date; set => date = value; }
+        public List<string> ImgLinks { get => imgLinks; set => imgLinks = value; }
+        public string Link { get => link; set => link = value; }
+
+        public static AvitoPost FormObject(string data)
+        {
+            return new AvitoPost
+            {
+                Name = data.FindElement(By.ItemProp("name"))[1].Value,
+                Price = data.FindElement(By.ItemProp("price"))[1].Value
+                      + Parce.FindElement(data, By.ItemProp("priceCurrency"))[0].Value.FindElement(By.Parametr("content"))[1].Value,
+                AdditionalInfo = data.FindElement(By.ClassName("specific-params"))[1].Value,
+                Date = data.FindElement(By.ClassName("js-item-date"))[0].Value.FindElement(By.Parametr("data-absolute-date"))[1].Value,
+                Link = @"https://www.avito.ru" + data.FindElement(By.ClassName("js-item-slider"))[0].Value.FindElement(By.Parametr("href"))[1].Value,
+                ImgLinks = (from Match arr in data.FindElements(By.regexForImageUrls)
+                            select arr.Groups[1].Value).ToList(),
+            };
+        }
+    }
+
+
+    
+
+}
+
+
+//dead zone
+//int indexStart = 0, indexEnd = 0;
+
+//string startSubStr = "itemprop=\"name\">";
+//string endSubStr = "<";
+
+//indexStart = data.IndexOf(startSubStr) + startSubStr.Length;
+//indexEnd = data.IndexOf(endSubStr, indexStart);
+
+//indexStart = FindStartInd(data, "itemprop=\"name\">");
+//indexEnd = FindEndInd(data, "<", indexStart);
+//item.Name = FormSubString(data, indexStart, indexEnd);
---
+++ AvitoParcer/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+
+namespace AvitoParcer
+{
+    public partial class Form1 : Form
+    {
+        ChromeDriver ChDriver;
+        public Form1()
+        {
+            InitializeComponent();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disk-cache-size=1", "--incognito");
+            ChDriver = new ChromeDriver(options);
+            ChDriver.Navigate().GoToUrl("https://www.avito.ru/istra/avtomobili/audi?radius=0");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> SeparatePageSourceCode(string pageSource)
+        {
+            List<string> item = new List<string>();
+            var temp = Parce.FindElements(pageSource, By.regexForPosts);
+            item = (from Match arr in temp
+                    select arr.Groups[0].Value).ToList();
+            return item;
+        }
+
+        public delegate void Method();
+        
+
+        private void WriterToJson(string data, int index, ref bool isCompleted)
+        {
+            AvitoPost avitoPost = AvitoPost.FormObject(data);
+
+            string fileName = "AvitoPost" + index + ".txt";
+            //File.WriteAllText(fileName, JsonConvert.SerializeObject(avitoPost, Formatting.Indented));
+            StreamWriter streamWriter = new StreamWriter(fileName);
+            streamWriter.Write(JsonConvert.SerializeObject(avitoPost, Formatting.Indented));
+            streamWriter.Close();
+
+            isCompleted = true;
+        }
+        private void WriterToJson(string data, int index, ref bool isCompleted, Method breaker)
+        {
+
+            breaker();
+            AvitoPost avitoPost = AvitoPost.FormObject(data);
+
+            string fileName = "AvitoPost" + index + ".txt";
+            //File.WriteAllText(fileName, JsonConvert.SerializeObject(avitoPost, Formatting.Indented));
+            StreamWriter streamWriter = new StreamWriter(fileName);
+            streamWriter.Write(JsonConvert.SerializeObject(avitoPost, Formatting.Indented));
+            streamWriter.Close();
+
+            isCompleted = true;
+        }
+        // static Task[] tasks;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pageCode = ChDriver.PageSource;
+
+            List<string> separatedPage = new List<string>();
+            separatedPage = SeparatePageSourceCode(pageCode);
+            bool[] isCompleted = new bool[separatedPage.Count()];
+            Task[] tasks = new Task[separatedPage.Count()];
+            for (int i = 0; i < separatedPage.Count(); i++)
+            {
+                int j = i;
+                isCompleted[i] = false;
+                if (!(j == 6  || j == 0))
+                    tasks[j] = Task.Factory.StartNew(() => WriterToJson(separatedPage[j], j, ref isCompleted[j]));
+                else
+                {
+                    tasks[j] = Task.Factory.StartNew((() => WriterToJson(separatedPage[j], j, ref isCompleted[j], (Method)(() => Thread.CurrentThread.Abort()))));
+                }
+            }
+
+            while (!tasks.All(task => task.IsCompleted) || !isCompleted.All(x => x))
+            {
+                for (int i = 0; i < separatedPage.Count(); i++)
+                {
+                    int j = i;
+
+                    if (tasks[j].IsFaulted && !isCompleted[j])
+                    {
+                        Task avaiableTask = (from task in tasks
+                                             where task.Status != TaskStatus.Faulted
+                                                || task.Status != TaskStatus.Canceled
+                                             select task).First();
+                        avaiableTask.ContinueWith(antecedent => WriterToJson(separatedPage[j], j, ref isCompleted[j]));
+                        Thread.Sleep(3000);
+                    }
+                }
+            }
+        }
+    }
+}
